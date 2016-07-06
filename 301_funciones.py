e0e658c74862a98eb0d004660eda0e098d94dc19
Cristofer [C] <cristofer.dev@gmail.com>
2016-07-06 18:15:02 -0400

    MODIFY 301 Funciones
    - Funciones basicas
    - Funciones con parametros
    - Funciones con parametros por defecto
    
    	modified:   301_funciones.py

--- 301_funciones.py
+++ 301_funciones.py
@@ -1,5 +1,22 @@
-def mi_funcion(p1, p2):
+# Con 
+def mi_funcion():
+	print("Funcion Basica sin parametros")
+
+
+mi_funcion()
+
+# Con parametros
+def p(p1, p2):
 	print(p1)
 	print(p2)
 
-mi_funcion("Hola", "Mundo")
+p("Hola", "Mundo")
+
+
+# Con parametros por defecto
+def otra_funcion(p1 = "Telefono", p2 = 5648961651):
+	print(p1)
+	print(p2)
+
+otra_funcion("Movil", 416548964)
+otra_funcion()
